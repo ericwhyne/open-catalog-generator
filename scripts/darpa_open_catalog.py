4042ab8077b7ecbbc290d33de2e4da067702765c
Tonya Cannariato <tonya.cannariato@gmail.com>
2014-03-04 12:05:35 -0500

    Update darpa_open_catalog.py
    Added Report a Problem line from Chris' 3/3 email. (Not sure if this one is necessary, or the one I did in the other pull request, so I'll let you sort out that detail.)

--- scripts/darpa_open_catalog.py
+++ scripts/darpa_open_catalog.py
@@ -15,6 +15,7 @@ def catalog_splash_content():
 <p>The Open Catalog is operating under the purview of the I2O XDATA program. With questions or concerns, please contact the Program Manager:<br>
 Dr. Christopher White<br>
 <a href='mailto:christopher.white@darpa.mil'>christopher.white@darpa.mil</a></p>
+<p>Report a problem: <a href="mailto:opencatalog@darpa.mil">opencatalog@darpa.mil</a></p>
 <p>Last updated: """ 
   splash += date + "</p>"
   return splash
