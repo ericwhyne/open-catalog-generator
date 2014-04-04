10ddd29724569797f2889f39ded53125cc5a4454
U-DT\snordt <snordt@W7x64-JWXT4S1.data-tactics-corp.com>
2014-04-04 16:59:41 -0400

    removal of json.dumps

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -202,15 +202,32 @@ for program in active_content:
           if "," in software['License']:
             licenses = software['License'].split(",")
             license_html = "<TD class=%s>" % column.lower()
-            for idx, val in enumerate(licenses):
-              if idx < len(licenses) - 1:
-                license_html += "<span onmouseover='licenseInfo(this, %s, \"%s\")'>%s</span>," % (json.dumps(license_content).replace("'", ""), val, val)
-              else:
-                license_html += "<span onmouseover='licenseInfo(this, %s, \"%s\")'>%s</span>" % (json.dumps(license_content).replace("'", ""), val, val)
+            for license_idx, license_val in enumerate(licenses):
+              license_value_found = False
+              for license_record in license_content:
+                for short_nm in license_record['License Short Name']:
+                  if license_val == short_nm:
+                    license_value_found = True
+                    if license_idx < len(licenses) - 1:
+                      license_html += "<span onmouseover='licenseInfo(this, \"%s\", \"%s\", \"%s\", \"%s\")'>%s</span>," % (short_nm, license_record['License Long Name'].replace("'", ""), license_record['License Link'], license_record['License Description'].replace("'", ""), license_val)
+                    else:					
+                      license_html += "<span onmouseover='licenseInfo(this, \"%s\", \"%s\", \"%s\", \"%s\")'>%s</span>" % (short_nm, license_record['License Long Name'].replace("'", ""), license_record['License Link'], license_record['License Description'].replace("'", ""), license_val)
+              if license_value_found == False:
+                if license_idx < len(licenses) - 1:
+                  license_html += "<span>%s,</span>" % license_val
+                else:
+                  license_html += "<span>%s</span>" % license_val
             program_page += license_html
             program_page += " </TD>\n </TR>\n"
-          else: 					
-           program_page += " <TD class=%s><span onmouseover='licenseInfo(this, %s, \"%s\")'>%s</span></TD>\n </TR>\n" % (column.lower(), json.dumps(license_content).replace("'", ""), software['License'], software['License'])
+          else:
+            license_value_found = False
+            for license_record in license_content: #license content json loop
+              for short_nm in license_record['License Short Name']:
+                if software['License'] == short_nm:
+                  license_value_found = True				
+                  program_page += " <TD class=%s><span onmouseover='licenseInfo(this, \"%s\", \"%s\", \"%s\", \"%s\")'>%s</span></TD>\n </TR>\n" % (column.lower(), short_nm, license_record['License Long Name'].replace("'", ""), license_record['License Link'], license_record['License Description'].replace("'", ""), software['License'])
+            if license_value_found == False:  
+              program_page += " <TD class=%s>%s</TD>\n </TR>\n" % (column.lower(), software['License'])
     program_page += doc.software_table_footer()
     program_page += "</div></div>"
 	
