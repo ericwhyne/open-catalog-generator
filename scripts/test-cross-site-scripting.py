17d27cbf8d0155d724009de64346d85a5660c864
James Tobat <tjd08a@gmail.com>
2014-05-16 13:49:40 -0400

    Updated script with new regex, detects all HTML tags

--- scripts/test-cross-site-scripting.py
+++ scripts/test-cross-site-scripting.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python
 # James Tobat, 2014
-from HTMLParser import HTMLParser
 import json
+import re
 import sys
 import glob
 import os
 
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-      HTMLParser.__init__(self)
-      self.found_tag = 0
-
-    def handle_starttag(self, tag, attrs):
-        if(tag != "br"):
-          self.found_tag += 1
-
-    def handle_endtag(self, tag):
-        self.found_tag += 1
-    
-    def handle_startendtag(self, tag, attrs):
-        if(tag != "br"):
-          self.found_tag += 1
-    
 # Finds the location in the JSON document of the script (line/col).
 # Also sends an exit code to the Makefile.
 def found_script_error(file_name, value):
@@ -33,18 +17,19 @@ def found_script_error(file_name, value):
     if column < 0:
       line_num+=1
     else:
-      print "\nERROR: Found embedded script/html in file %s, line %i column %i" % (name_nopath, line_num, column) 
-      sys.exit(1)
+      print "\nERROR: Found embedded html in file %s, line %i column %i" % (name_nopath, line_num, column)
   error_file.close()
   return
   
-# Identifies if the given value (string) has a html tags inside of it
-# Will pick any html tags even self containing html.
+# Identifies if the given value (string) has an html tags inside of it.
+# Will pick out html tags that have single value <h> or <img /> as well
 def is_html(value):
-  parser = MyHTMLParser()
-  parser.feed(value)
-  html = parser.found_tag
-  if html:
+  # will ignore br html tags, can be commented out
+  # value = value.replace('<br>',' ') 
+  # value = value.replace('<br/>',' ')
+  html_opentag = re.search('<[a-zA-Z][^>]*>', value, re.I)
+  html_closetag = re.search('</[a-zA-Z][^>]*>', value, re.I)
+  if html_opentag or html_closetag:
     return value
   else:
     return 0
@@ -52,7 +37,7 @@ def is_html(value):
 # Iterates through a json object (all keys/values) and
 # tests each value to see if it has a script inside of it.
 def test_for_xss(json_data, identifier):
-  parser = MyHTMLParser()
+  found_error = 0
   for key, value in json_data.iteritems():
     html = 0
     if isinstance(value, basestring) and value != "":
@@ -60,18 +45,21 @@ def test_for_xss(json_data, identifier):
     elif isinstance(value, list):
       for item in value:
         if(item != ""):
-	  html = is_html(item) 
+          html = is_html(item)
 
     if html:
       found_script_error(identifier, html)
+      found_error = 1
 
-  return 
+  return found_error
 
 data_dir = sys.argv[1]
 path = data_dir + '/*.json'
 
 # Iterates through all JSON files in the directory
 # provided to the script.
+found_errors = 0
+
 for file_name in glob.glob(path):
   name_nopath = os.path.basename(file_name)
   json_file = open(file_name)
@@ -84,8 +72,12 @@ for file_name in glob.glob(path):
 
   json_file.close()
   if isinstance(json_content, dict):
-    test_for_xss(json_content, file_name)
+    found_errors += test_for_xss(json_content, file_name)
   else:
     for record in json_content:
-      test_for_xss(record, file_name)
+      found_errors += test_for_xss(record, file_name)
+
+if found_errors:
+  sys.exit(1)
+
   
