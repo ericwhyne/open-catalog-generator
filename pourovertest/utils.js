29a0f69d011d55ad3fba998b638c372c5646ff41
U-DT\snordt <snordt@W7x64-JWXT4S1.data-tactics-corp.com>
2014-05-30 17:54:29 -0400

    initial commit of query center

---
+++ pourovertest/utils.js
@@ -0,0 +1,59 @@
+
+function getPrograms() {
+	var programs;
+	$.ajax({
+		async: false,
+		url: 'active_content.json',
+		dataType: 'json',
+		success: function(response){
+		   programs = response;
+		}
+	});
+	
+	return programs;
+}
+
+function getProgramDetails(filename) {
+	var data;
+	$.ajax({
+		async: false,
+		url: filename,
+		dataType: 'json',
+		success: function(response){
+		   data = response;
+		}
+	});
+	return data;
+}
+
+function isInArray(value, array) {
+  //checks to see if a value exists in an array
+  return array.indexOf(value) > -1;
+}
+
+function isIE() {
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf("MSIE ");
+	//if IE browser, return true. If another browser, return false
+	if (msie > 0)      
+		return true;
+	else    
+		return false;
+}
+
+function sortByProperty(property) {
+	//sorts json array by a given property name
+    return function (a, b) {
+        var sortStatus = 0;
+        if (a[property].toLowerCase().trim() < b[property].toLowerCase().trim())
+            sortStatus = -1;
+        else if (a[property].toLowerCase().trim() > b[property].toLowerCase().trim())
+            sortStatus = 1;
+ 
+        return sortStatus;
+    };
+}
+
+function getStringArray(object) {
+	return object = typeof object == 'string' ? [object] : object;
+}
