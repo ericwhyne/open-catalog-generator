a0a0c859fbf59188ed8c27496b35ea36754b0608
Eric Whyne <ericwhyne@gmail.com>
2014-06-03 17:50:32 -0400

    stats_gen

---
+++ stats_gen/_get_hard-to-get_code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd code
+
+mkdir immersive_body-based_interactions ; cd immersive_body-based_interactions
+git svn init -s http://svnmimir.googlecode.com/svn/ ; git svn fetch &
+cd ..
+
+mkdir oodt ; cd oodt
+git svn init -s https://svn.apache.org/repos/asf/oodt/ ; git svn fetch &
+cd ..
+
+mkdir bigdata ; cd bigdata
+git svn init -s https://bigdata.svn.sourceforge.net/svnroot/bigdata/ ; git svn fetch &
+cd ..
+
+mkdir mpgraph; cd mpgraph
+git svn init -s http://svn.code.sf.net/p/mpgraph/code/ ; git svn fetch &
+cd ..
